b2309cb8a0728003be32a4a66f6339b07f30c975
Your Name <you@example.com>
2021-11-29 14:16:13 -0300

    Update meuscript.js

--- Git Hub Users/meuscript.js
+++ Git Hub Users/meuscript.js
@@ -40,6 +40,27 @@ function Tabela() {
     const ColunaBotao = document.createElement('th');
     table.id = "myTable"
 
+    function myFunction() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+      
+      
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }
+        }
+      }
+
     ColunaNome.innerText = 'ColunaNome';
     ColunaUsername.innerText = 'ColunaUser';
     ColunaBotao.innerText = 'ColunaBotao';
