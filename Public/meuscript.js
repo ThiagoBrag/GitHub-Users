82a5cc94d5ba9fc41bb1e409a633728ade08c457
Your Name <you@example.com>
2021-11-30 19:53:23 -0300

    Update meuscript.js

--- Public/meuscript.js
+++ Public/meuscript.js
@@ -53,8 +53,17 @@ function Tabela() {
 
         for (i = 0; i < tr.length; i++) {
           td = tr[i].getElementsByTagName("td")[0];
+          td2 = tr[i].getElementsByTagName("td")[1];
           if (td) {
             txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filtro) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }
+          if (td2) {
+            txtValue = td2.textContent || td2.innerText;
             if (txtValue.toUpperCase().indexOf(filtro) > -1) {
               tr[i].style.display = "";
             } else {
