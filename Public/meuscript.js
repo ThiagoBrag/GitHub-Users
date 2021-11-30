555d54c70c894bc98d863ab5c444628039ba2147
Your Name <you@example.com>
2021-11-30 19:30:34 -0300

    Update meuscript.js

--- Public/meuscript.js
+++ Public/meuscript.js
@@ -45,7 +45,7 @@ function Tabela() {
 
     inputFiltro.onkeyup = myFunction
     function myFunction() {
-        var input, filter, table, tr, td, td2, i, txtValue;
+        var input, filter, table, tr, td, i, txtValue;
         input = document.getElementById("myInput");
         filter = input.value.toUpperCase();
         table = document.getElementById("myTable");
@@ -53,7 +53,6 @@ function Tabela() {
 
         for (i = 0; i < tr.length; i++) {
           td = tr[i].getElementsByTagName("td")[0];
-          td2 = tr[i].getElementsByTagName("td")[0];
           if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -62,16 +61,8 @@ function Tabela() {
               tr[i].style.display = "none";
             }
           }
-          if (td2) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              tr[i].style.display = "";
-            } else {
-              tr[i].style.display = "none";
-            }
-          }
         }
-
+    }
     ColunaNome.innerText = 'ColunaNome';
     ColunaUsername.innerText = 'ColunaUser';
     ColunaBotao.innerText = 'ColunaBotao';
