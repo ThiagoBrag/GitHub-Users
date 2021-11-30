c80a219ce8b136df2d7df01b3bd3179a7f703a6d
Your Name <you@example.com>
2021-11-30 19:13:44 -0300

    Update meuscript.js

--- Public/meuscript.js
+++ Public/meuscript.js
@@ -63,6 +63,18 @@ function Tabela() {
             }
           }
         }
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+              txtValue = td.textContent || td.innerText;
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            }
+          }
+        }
       }
 
     ColunaNome.innerText = 'ColunaNome';
